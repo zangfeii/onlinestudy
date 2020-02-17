9be6690159434f90472244a2cbd9510cf2c5beb0
ZAHNGFEI <ainiyou.xls@gmail.com>
2020-02-17 13:57:10 +0800

    项目文件搭建

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <div id="login">
+   <h1>登陆界面</h1>
+  </div>  
 </template>
 
 <script>
 export default {
-  name: 'App',
+
 }
 </script>
 
 <style>
- 
+
 </style>
